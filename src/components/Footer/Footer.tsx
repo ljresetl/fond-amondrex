e9c208880ecfc66965094ebcd4ff66a6c304dd8c
jres <ljresetl@gmail.com>
2025-12-21 11:58:06 +0100

    header-sticky

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -33,12 +33,38 @@ const Footer: React.FC = () => {
        </div>
               <div className={styles.right_all}>
                   <div>
-  <ul className={styles.socials}>
-    <li><a href="#"><SiTiktok size={24} color="#e5f9ff" /></a></li>
-    <li><a href="#"><SiInstagram size={24} color="#e5f9ff" /></a></li>
-    {/* <li><a href="#"><SiFacebook size={24} color="#e5f9ff" /></a></li> */}
-    <li><a href="#"><SiThreads size={24} color="#e5f9ff" /></a></li>
-  </ul>
+<ul className={styles.socials}>
+  <li>
+    <a 
+      href="https://www.tiktok.com/@amondrexcf?_r=1&_t=ZM-92P9VXO9Ict" 
+      target="_blank" 
+      rel="noopener noreferrer"
+    >
+      <SiTiktok size={24} color="#e5f9ff" />
+    </a>
+  </li>
+
+  <li>
+    <a 
+      href="https://www.instagram.com/amondrexcf?igsh=dThqMG5hNTlkYWoy" 
+      target="_blank" 
+      rel="noopener noreferrer"
+    >
+      <SiInstagram size={24} color="#e5f9ff" />
+    </a>
+  </li>
+
+  <li>
+    <a 
+      href="https://www.threads.com/@amondrexcf" 
+      target="_blank" 
+      rel="noopener noreferrer"
+    >
+      <SiThreads size={24} color="#e5f9ff" />
+    </a>
+  </li>
+</ul>
+
 </div>
 
             <div className={styles.right}>
